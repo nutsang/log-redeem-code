51d8b4fdbb8f7304f95e8dd2fdd6fed899b72d51
NutSang Official <90796502+nutsang@users.noreply.github.com>
2024-03-06 19:37:22 +0700

    Add files via upload

---
+++ log-redeem-code.lua
@@ -0,0 +1,32 @@
+local HttpService = game:GetService("HttpService")
+local ServerStorage = game:GetService("ServerStorage")
+local MarketplaceService = game:GetService("MarketplaceService")
+local GAME_NAME = MarketplaceService:GetProductInfo(game.PlaceId)["Name"]
+local ITEM_REDEEM_CODE = ServerStorage:FindFirstChild('item-redeem-code', false)
+repeat
+	if ITEM_REDEEM_CODE then
+		local descendants = ITEM_REDEEM_CODE:GetDescendants()
+		local redeemCode = {}
+		for index, descendant in pairs(descendants) do
+			if descendant:IsA("StringValue") then
+				if descendant.Name == "redeem-code" then
+					redeemCode[#redeemCode+1] = descendant.Value
+				end
+			end
+		end
+		local requestOption = {
+			Url = "http://127.0.0.1:3001/redeem-code-log",
+			Method = "POST",
+			Headers = {
+				["Content-Type"] = "application/json",
+			},
+			Body = HttpService:JSONEncode({[GAME_NAME] = redeemCode})
+		}
+		pcall(function()
+			HttpService:RequestAsync(requestOption)
+		end)
+	else
+		warn("Please create folder item-redeem-code in ServerStorage.")
+	end
+	task.wait(20)
+until false
